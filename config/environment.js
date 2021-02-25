d9e02a43589c8bba627d1e03b436b595fd2989d2
Daniel Fernandez De Ascencao <dfernandezda21@gmail.com>
2021-02-25 17:20:10 +0000

    last commit before deploying on heroku

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/pokezon'
-export const secret = 'this string is a special string'
+import dotenv from 'dotenv'
+dotenv.config()
 
-
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/<name-of-your-db>'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from 'express'
-
-import logger from './lib/logger.js'
 import { port } from './config/environment.js'
 import connectToDatabase from './lib/connectToDb.js'
+import logger from './lib/logger.js'
 import router from './config/router.js'
 import errorHandler from './lib/errorHandler.js'
+import path from 'path' // * <—- a new import from node
+
 
 const app = express()
+
+const __dirname = path.resolve() // * this line has been added, note this has a double underscore before it
 
 async function startServer() {
   try {
     await connectToDatabase()
-    console.log(' Database has connected')
+
+    console.log('🤖 Database has connected')
+
+    app.use(express.static(`${__dirname}/client/build`)) // * <— This line has been added before the express json middleware, it will allow the app to respond to a request with contents of this directory “build”, which will contain our React App code.
 
     app.use(express.json())
 
@@ -19,14 +25,15 @@ async function startServer() {
 
     app.use('/api', router)
 
-    //error handler needed here
+    app.use('/*', (_, res) => res.sendFile(`${__dirname}/client/build/index.html`)) // * <— This additional route handler has been added between the router and error handler middleware, it means that any incoming request that does not match a route in router should respond back with our frontend.
 
     app.use(errorHandler)
 
-    app.listen(port, () => console.log(`Up and running on port ${port}`))
+    app.listen(port, () => console.log(`🤖 Up and running on port ${port}`))
   } catch (err) {
     console.log('🤖 Something went wrong starting the App')
     console.log(err)
   }
 }
+
 startServer()
